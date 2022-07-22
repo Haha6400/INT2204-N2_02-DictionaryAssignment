3803290c28f8e1b690305728317ca0d103162c29
ducqh8a <108819964+ducqh8a@users.noreply.github.com>
2022-07-22 07:05:47 +0700

    Delete BTL Tuan1/src directory

--- BTL Tuan1/src/Dictionary.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class Dictionary {
-        public static ArrayList<Word> dictionary =new ArrayList<Word>();
-        public static HashMap<String,String> hoTroTraCuu=new HashMap<String,String>();
-
-    }
