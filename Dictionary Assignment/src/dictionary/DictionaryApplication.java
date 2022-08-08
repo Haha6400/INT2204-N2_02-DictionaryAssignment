c1d85556853d72feba3c89c0d60520b16701351c
ducqh8a <108819964+ducqh8a@users.noreply.github.com>
2022-08-08 10:33:59 +0700

    Add files via upload

---
+++ Dictionary Assignment/src/dictionary/DictionaryApplication.java
@@ -0,0 +1,49 @@
+package dictionary;
+
+import dictionary.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DictionaryApplication {
+
+    private MultiDictionary mdict = new MultiDictionary("res\\mdxs.txt");
+    private DefaultDictionary ddict = new DefaultDictionary();
+
+    public DictionaryApplication() throws IOException {
+        ddict.insertFromFile();
+    }
+
+    public MultiDictionary getMdict() {
+        return mdict;
+    }
+    public DefaultDictionary getDdict() {return ddict; }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("START: " + "\n");
+        String key = "hell";
+
+        MultiDictionary dict = new MultiDictionary("res\\mdxs.txt");
+        System.out.println("TEST: " + "\n");
+
+
+        System.out.println("LOOK UP TEST: " + "\n");
+        ArrayList<String> wordExplains = dict.lookUp(key);
+        for (int i = 0; i < wordExplains.size(); i++) {
+            System.out.println(wordExplains.get(i));
+        }
+
+        System.out.println("SEARCHER TEST: " + "\n");
+        ArrayList<String> wordTargets = dict.searcher(key);
+        for (int i = 0; i < wordTargets.size(); i++) {
+            System.out.println(wordTargets.get(i));
+        }
+
+        System.out.println("ADJACENT TEST: " + "\n");
+        ArrayList<String> words = dict.adjacentWord(key, 10);
+        for (int i = 0; i < words.size(); i++) {
+            System.out.println(words.get(i));
+        }
+        System.out.println("END." + "\n");
+    }
+}
